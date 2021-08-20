a0d70f9dd1bb8fc68741bbcf086ae5951af9b81c
seu11ee <yslee2504@naver.com>
2021-08-21 01:29:34 +0900

    210821

---
+++ Book/CH9/미래도시.py
@@ -0,0 +1,19 @@
+INF = int(1e9)
+n,m = map(int,input().split())
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+x,k = map(int,input().split())
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+answer = graph[1][k]+graph[k][x]
+if answer >= INF:
+    print(-1)
+else:
+    print(answer)
+
+
