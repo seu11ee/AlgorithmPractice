d289d8dc578ee0929c263a1d51fdf545128c41b1
seu11ee <yslee2504@naver.com>
2021-09-15 00:33:22 +0900

    210914

--- BaekJoon/10819_차이를최대로.py
+++ BaekJoon/10819_차이를최대로.py
@@ -1,17 +1,13 @@
 import sys
-from collections import deque
+from itertools import permutations
 n = int(sys.stdin.readline())
-A = deque(map(int, sys.stdin.readline().split()))
-sorted(A)
-sum=0
-while(A):
-    a = A.pop()
-    if (A):
-        b = A.popleft()
-    else:
-        sum +=
-    if (A):
-        c = A.pop()
-    A.append(c)
-    sum = sum + abs(a-b) +abs(b-c)
-print(sum)
+A = list(map(int, sys.stdin.readline().split()))
+max_sum = 0
+for numbers in permutations(A):
+    sub_sum = 0
+    for i in range(n-1):
+        sub_sum += abs(numbers[i]-numbers[i+1])
+    if sub_sum > max_sum:
+        max_sum = sub_sum
+
+print(max_sum)
